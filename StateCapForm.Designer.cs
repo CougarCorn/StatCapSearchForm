c6fac5bb0c9a622bd240c9df2c2839dbf0717a83
Tanner Williams <10773643@uvu.edu>
2020-01-15 17:12:21 -0700

    Modified Windows Form and Started the Console App

---
+++ StateCapForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace StatCapSearchForm
+{
+    partial class StateCapForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StateTxtBox = new System.Windows.Forms.TextBox();
+            this.CapitalTxtBox = new System.Windows.Forms.TextBox();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LstBoxStateCap = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // StateTxtBox
+            // 
+            this.StateTxtBox.Location = new System.Drawing.Point(169, 85);
+            this.StateTxtBox.Name = "StateTxtBox";
+            this.StateTxtBox.Size = new System.Drawing.Size(161, 26);
+            this.StateTxtBox.TabIndex = 0;
+            // 
+            // CapitalTxtBox
+            // 
+            this.CapitalTxtBox.Location = new System.Drawing.Point(169, 144);
+            this.CapitalTxtBox.Name = "CapitalTxtBox";
+            this.CapitalTxtBox.Size = new System.Drawing.Size(161, 26);
+            this.CapitalTxtBox.TabIndex = 1;
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Location = new System.Drawing.Point(177, 195);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(153, 45);
+            this.ButtonSave.TabIndex = 3;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            this.ButtonSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "State";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Capital";
+            // 
+            // LstBoxStateCap
+            // 
+            this.LstBoxStateCap.HideSelection = false;
+            this.LstBoxStateCap.Location = new System.Drawing.Point(436, 85);
+            this.LstBoxStateCap.Name = "LstBoxStateCap";
+            this.LstBoxStateCap.Size = new System.Drawing.Size(267, 294);
+            this.LstBoxStateCap.TabIndex = 5;
+            this.LstBoxStateCap.TabStop = false;
+            this.LstBoxStateCap.UseCompatibleStateImageBehavior = false;
+            // 
+            // StateCapForm
+            // 
+            this.AcceptButton = this.ButtonSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LstBoxStateCap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.CapitalTxtBox);
+            this.Controls.Add(this.StateTxtBox);
+            this.Name = "StateCapForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.StateCapForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox StateTxtBox;
+        private System.Windows.Forms.TextBox CapitalTxtBox;
+        private System.Windows.Forms.Button ButtonSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView LstBoxStateCap;
+    }
+}
+
---
+++ StateCapForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatCapSearchForm
+{
+    public partial class StateCapForm : Form
+    {
+        //
+        private string filename = "statecap.txt";
+
+        //
+        public StateCapForm()
+        {
+            //
+            InitializeComponent();
+        }
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+            string userState = StateTxtBox.Text;
+            string userCapital = CapitalTxtBox.Text;
+            if (userState != "" && userCapital != "")
+            {
+                try
+                {
+                    //
+                    StreamWriter outputFile = File.AppendText(filename);
+                    //
+                    outputFile.WriteLine($"{userState},{userCapital}");
+                    //
+                    outputFile.Close();
+                    //
+                    LstBoxStateCap.Items.Add($"{userState},{userCapital}");
+
+                    CapitalTxtBox.Clear();
+                    StateTxtBox.Clear();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            
+        }
+        //
+        private void StateCapForm_Load(object sender, EventArgs e)
+        {
+            //Usine StreamReader to try and open a file
+            StreamReader inputfile;
+            string[] tempRead;
+
+
+            try
+            {
+                //Load and open the file
+                inputfile = File.OpenText(filename);
+                //Skip the headers
+                inputfile.ReadLine();
+                while (!inputfile.EndOfStream)
+                {
+                    //
+                    tempRead = inputfile.ReadLine().Split(',');
+                    //Put it into the list box
+                    LstBoxStateCap.Items.Add($"{tempRead[0]},{tempRead[1]}");
+                }
+                //
+                inputfile.Close();
+            }
+            catch (Exception ex)
+            {
+                //
+                MessageBox.Show(ex.Message);
+            }
+                 else
+            {
+                MessageBox.Show("Invalid Input", "Please enter a State and Capital.");
+            }
+        }
+    }
+}
